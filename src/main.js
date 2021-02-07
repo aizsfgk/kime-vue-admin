eea68a1ff017692c1ad2b708aabb9661fdc5ce47
hadoop <1732756017@qq.com>
2021-02-07 20:26:22 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
+// 导入必要的文件
 import Vue from 'vue'
+// 导入css文件
+import 'normalize.css/normalize.css'
+// 导入element模板
+import ElementUI from 'element-ui'
+// element css
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+// 导入全局css
+import '@/styles/index.scss'
+
+
+// 导入本地文件
 import App from './App.vue'
 import store from './store'
+import router from './router'
+
+
+console.log(process.env)
+
+// 使用ElementUI组件库
+Vue.use(ElementUI)
+
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,34 @@
+'use strict'
+
+const path = require('path')
+const defaultSettings = require('./src/settings.js')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const name = defaultSettings.title || 'Kime Vue Admin'
+
+const port = process.env.port || process.env.npm_config_port || 9988
+
 module.exports = {
-  lintOnSave: false
+  // 部署应用包时的基本URL
+  publicPath: '/',
+  // 当运行 vue-cli-service build 时生成的生产环境构建文件的目录
+  outputDir: 'dist',
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetDir: 'static',
+  // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码。这个值会在 @vue/cli-plugin-eslint 被安装之后生效
+  lintOnSave: process.env.NODE_ENV === 'development',
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
+  productionSourceMap: false,
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warning: false,
+      errors: true
+    }
+  }
 }
